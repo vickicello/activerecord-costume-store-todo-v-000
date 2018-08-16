d37dc6a78f7f50da37e43278398b92a90eae29d1
Vicki Hamm <vicki.cello@gmail.com>
2018-08-16 22:34:38 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -33,4 +33,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.integer "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
