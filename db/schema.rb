a908be58d4e41f29db23441f85088a10268a183e
Vicki Hamm <vicki.cello@gmail.com>
2018-08-16 22:33:54 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
